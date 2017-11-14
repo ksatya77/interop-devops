8c4844e70aa46ba8c3e31ccd793094d1db9db3d0
jeffplesko <jeffplesko@gmail.com>
2017-11-13 18:09:01 -0600

    Added mojaloopctl script for proper vagrant startup/restart
    Usage:  mojaloopctl init | start | restart [all|cst|dfsps|dfsp1|dfsp2] | stop | remove

--- startup.sh
+++ startup.sh
@@ -1,2 +1,29 @@
 #! /bin/bash
-for k in `sudo docker ps -a --format {{.ID}}`; do sudo docker start $k; done
+
+box=`hostname`
+
+case $box in
+cst)
+  sudo /opt/mule/bin/mule stop
+  echo "Starting Database...\n"
+  for k in `sudo docker ps -a --format {{.ID}}:{{.Image}} | grep postgres | cut -d: -f1`; do sudo docker restart $k; done
+  echo "Starting Mojaloop IST components..\n"
+  for k in `sudo docker ps -a --format {{.ID}}`; do sudo docker start $k; done
+  sudo /opt/mule/bin/mule start
+  ;;
+mgmt)
+  echo "Nothing to do..\n"
+  ;;
+*)
+  sudo /opt/mule/bin/mule stop
+  echo "Starting Database...\n"
+  for k in `sudo docker ps -a --format {{.ID}}:{{.Image}} | grep postgres | cut -d: -f1`; do sudo docker restart $k; done
+  echo "Starting Mojaloop DFSP components..\n"
+  for k in `sudo docker ps -a --format {{.ID}}:{{.Image}} | grep dfsp | cut -d: -f1`; do sudo docker restart $k; done
+  sudo /opt/mule/bin/mule start
+  echo "Pausing for 30 seconds to allow mule to start...\n"
+  sleep 30
+  echo "Starting Mojaloop ILP components..\n"
+  for k in `sudo docker ps -a --format {{.ID}}:{{.Image}} | grep ilp | cut -d: -f1`; do sudo docker restart $k; done;
+  ;;
+esac
--- vagrant_startup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-vagrant ssh dfsp1 -c "~/startup.sh"
-vagrant ssh dfsp1 -c "~/startup.sh"
-vagrant ssh dfsp2 -c "~/startup.sh"
-vagrant ssh dfsp2 -c "~/startup.sh"
-vagrant ssh cst -c "~/startup.sh"
-vagrant ssh cst -c "~/startup.sh"
